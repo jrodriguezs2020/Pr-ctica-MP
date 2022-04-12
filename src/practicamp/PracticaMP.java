256b3ff911717d37909ebc70f07d14569de1643f
Victo <Victo@DESKTOP-GKPR6RU>
2022-04-12 15:32:57 +0200

    Interfaz gráfica de inicio de sesión

--- src/practicamp/PracticaMP.java
+++ src/practicamp/PracticaMP.java
@@ -4,7 +4,14 @@ package practicamp;
 public class PracticaMP {
 
     public static void main(String[] args) {
-     
+        /*String nombre;
+        String contraseña;
+        ventanas.InterfaceRegistro ventanaRegistro = new ventanas.InterfaceRegistro();
+        int num = ventanaRegistro.getAccion();
+        if (num == 1) {
+            nombre = ventanaRegistro.getNombre();
+            contraseña = ventanaRegistro.getContraseña();
+        } */
     }
     
 }
