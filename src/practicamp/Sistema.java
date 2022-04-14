9af9298cee18c5cecb7f8da14be18921636c7c33
noeli <noelia>
2022-04-14 21:32:52 +0200

    Hemos creado una interfaz para que se elija si eres usuario u operador. Para esto, hemos creado otro fichero llamado  "ListaOperadores.txt" en el que se guardan los operadores. Hemos añadido a lo que estaba hecho que un operador se pueda registrar e iniciar sesion.

--- src/practicamp/Sistema.java
+++ src/practicamp/Sistema.java
@@ -16,46 +16,82 @@ import java.util.Iterator;
 
 public class Sistema {
     ArrayList<Usuario> usuariosList;
+    ArrayList<Operador> operadorList;
 
     
    
         public void inicio() throws FileNotFoundException, IOException, ClassNotFoundException{
-        ventanas.inicioSesion inicio = new ventanas.inicioSesion(this);
-        inicio.setVisible(true);
+        ventanas.Usuario_Operador usuOpe =new ventanas.Usuario_Operador(this);
+        usuOpe.setVisible(true);
+        
+        //ventanas.inicioSesion inicio = new ventanas.inicioSesion(this);
+        //inicio.setVisible(true);
         usuariosList = new ArrayList();
-        FileInputStream fileStream = new FileInputStream("..\\ListaUsuarios.txt");
-        ObjectInputStream objectStream = new ObjectInputStream(fileStream);
-        usuariosList= (ArrayList<Usuario>) objectStream.readObject();
+        operadorList=new ArrayList();
+        FileInputStream fileStreamU = new FileInputStream("..\\ListaUsuarios.txt");
+        ObjectInputStream objectStreamU = new ObjectInputStream(fileStreamU);
+        usuariosList= (ArrayList<Usuario>) objectStreamU.readObject();
+        FileInputStream fileStreamO = new FileInputStream("..\\ListaOperadores.txt");
+        ObjectInputStream objectStreamO = new ObjectInputStream(fileStreamO);
+        operadorList= (ArrayList<Operador>) objectStreamO.readObject();
           
         }
-    public boolean usuarioCorrecto(String nick, String contraseña){
+    public boolean personaCorrecta(String nick, String contraseña, int modo){
         boolean result;
+        if (modo==1){
         Usuario usuario2 = new Usuario(nick,contraseña);
         if(usuariosList.contains(usuario2)){
            result=true;
         }
         else{
            result=false; 
+        }}
+        else{
+            Operador operador2 = new Operador(nick,contraseña);
+            if(operadorList.contains(operador2)){
+           result=true;
+        }
+        else{
+           result=false; 
+        }
         }
         return result;
     }
     
-    public void nuevoUsuario(String nombre, String nick,String contraseña) throws FileNotFoundException, IOException{
+    public void nuevaPersona(String nombre, String nick,String contraseña, int modo) throws FileNotFoundException, IOException{
+        if(modo==1){
         Usuario usuario = new Usuario(nombre,nick,contraseña);
         usuariosList.add(usuario);
-        FileOutputStream fileStream = new FileOutputStream("..\\ListaUsuarios.txt");
-        ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
-        objectStream.writeObject(usuariosList);
-        objectStream.close();
+        FileOutputStream fileStreamU = new FileOutputStream("..\\ListaUsuarios.txt");
+        ObjectOutputStream objectStreamU = new ObjectOutputStream(fileStreamU);
+        objectStreamU.writeObject(usuariosList);
+        objectStreamU.close();
+        }
+        if(modo==0){
+        Operador operador=new Operador(nombre,nick,contraseña);
+        operadorList.add(operador);
+        FileOutputStream fileStreamO = new FileOutputStream("..\\ListaOperadores.txt");
+        ObjectOutputStream objectStreamO = new ObjectOutputStream(fileStreamO);
+        objectStreamO.writeObject(operadorList);
+        objectStreamO.close();
+        }
+       
     
     }
     
-    public boolean comprobarNick (String nick) {
+     
+    public boolean comprobarNick (String nick, int modo) {
+        if (modo==1){
         for (Usuario i : usuariosList) {
             if (i.getNick().equals(nick)) {
                 return true;
             }
-        }
+        }}else{
+            for (Operador j : operadorList) {
+            if (j.getNick().equals(nick)) {
+                return true;
+            }
+        }}
         return false;
     }
     public boolean darBaja(Usuario usuario) throws FileNotFoundException, IOException {
