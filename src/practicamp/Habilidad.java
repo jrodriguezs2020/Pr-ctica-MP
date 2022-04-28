9bc969b66308d7ca13416efadf2beee0caf04e9a
lauraramiro <102876114+lauraramiro@users.noreply.github.com>
2022-04-28 18:19:27 +0200

    Habilidades especiales

--- src/practicamp/Habilidad.java
+++ src/practicamp/Habilidad.java
@@ -1,8 +1,42 @@
 
 package practicamp;
 
-public abstract class Habilidad {
+import java.io.Serializable;
+
+public abstract class Habilidad implements Serializable{
      private String nombre;
      private int valorAtaque;
      private int valorDefensa;
+     
+     public Habilidad(){
+         this.nombre = "";
+         valorAtaque = Math.toIntExact(Math.round(Math.random()*2+1));
+         valorDefensa = Math.toIntExact(Math.round(Math.random()*2+1));
+     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getValorAtaque() {
+        return valorAtaque;
+    }
+
+    public void setValorAtaque(int valorAtaque) {
+        this.valorAtaque = valorAtaque;
+    }
+
+    public int getValorDefensa() {
+        return valorDefensa;
+    }
+
+    public void setValorDefensa(int valorDefensa) {
+        this.valorDefensa = valorDefensa;
+    }
+     
+     
 }
